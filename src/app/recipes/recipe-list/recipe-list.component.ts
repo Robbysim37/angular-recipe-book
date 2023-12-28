0f073bb91542b21f246d9986849a19ea99d0e15b
Robert Lewis <Robbysim37@gmail.com>
2023-12-28 17:26:54 -0500

    added dynamic ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
+  recipes: Recipe[] = [
+    new Recipe("A test Recipe","This is a test","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeQinDBJx3QMgwn1B5bB9eTjQvhsPUEP5OYw&usqp=CAU")
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
